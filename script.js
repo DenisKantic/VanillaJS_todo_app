34ac26e0d44da10679469aa7e12fc0d3e86c2530
DenisKantic <denis.kantic18@gmail.com>
2020-05-16 07:46:57 +0200

    finished with css and javascript

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const todoList = document.querySelector('.todo-list');
 // event listeners
 
 todoButton.addEventListener('click', createItem);
+todoList.addEventListener('click', removeItem);
 
 
 // functions
@@ -43,4 +44,19 @@ function createItem(event){
     todoList.appendChild(todoDiv);
 
     todoInput.value="";
+}
+
+function removeItem(e){
+
+    const item=e.target;
+
+    if(item.classList[0]==='trash-btn'){
+        const todo = item.parentElement;
+        todo.remove();
+    }
+
+    if(item.classList[0]==='check-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
 }
