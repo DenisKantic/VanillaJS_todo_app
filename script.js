d564bc6e9f5c854084936a7e2385eb77eae6daa7
DenisKantic <denis.kantic18@gmail.com>
2020-05-16 06:49:19 +0200

    created const for classes in html, added event listener for createItem, and added function createItem

---
+++ script.js
@@ -0,0 +1,19 @@
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.submit-btn');
+const todoList = document.querySelector('.todo-list');
+
+
+// event listeners
+
+todoButton.addEventListener('click', createItem);
+
+
+// functions
+
+function createItem(event){
+
+
+    event.preventDefault();
+
+    
+}
